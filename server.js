1c0d09ae435e0dce4870287e1a0470dcabf5c196
Mariyam Salim <mariyamsalim03@gmail.com>
2025-12-09 00:36:16 +0400

    image serving function added

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
 
 app.use(cors());
 
@@ -9,6 +11,18 @@ app.use(express.json());
 app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
     next(); 
+});
+
+app.use('/images', (req, res, next) => {
+    const filePath = path.join(__dirname, 'images', req.url);
+    fs.stat(filePath, (err, fileInfo) => {
+        if (err) {
+            res.status(404).send("Image not found");
+            return;
+        }
+        if (fileInfo.isFile()) res.sendFile(filePath);
+        else next();
+    });
 });
 
 const PORT = 3000;
